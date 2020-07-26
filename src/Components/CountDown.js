af32a1c0dc1e0f49aeab9db25f5e5f50709ce93d
Renata Santoso <santoso.renata@gmail.com>
2020-07-25 23:44:58 -0700

    Add tick functionality

--- src/Components/CountDown.js
+++ src/Components/CountDown.js
@@ -7,20 +7,53 @@ const CountDown = () => {
    const [minutes, setMinutes] = useState(0);
    const [seconds, setSeconds] = useState(0);
    const [isActive, setIsActive] = useState(false);
+   const [isOver, setIsOver] = useState(false);
+   
+   let timeLeft;
 
    const handleInput = (e) => {
       e.preventDefault();
 
-      let mins = Number(e.target.value);      
+      let mins = Number(e.target.value);
+      setError(validate(mins));
       setStartInMinutes(mins);
       setMinutes(mins);
    };
+
+   const tick = () => {
+      timeLeft = minutes * 60 + seconds;
+
+      timeLeft--;
+      if (timeLeft < 0) {
+         setIsOver(true);
+      } else {
+         let min = Math.floor(timeLeft / 60);
+         setMinutes(min);
+         setSeconds(timeLeft - min * 60);
+         checkTimeLeft();
+      }
+   };
+
+   useEffect(() => {
+      let timerId;
+      let speed = 1000 / Number(selectedSpeed);
+
+      if (isActive) {
+         timerId = setInterval(() => {
+            tick();
+         }, speed);
+      } else if (!isActive && minutes > 0 && seconds > 0) {
+         clearInterval(timerId);
+      }
+
+      return () => clearInterval(timerId);
+   });
 
    return (
       <div>
          <label htmlFor='timeInput'>CountDown: </label>         
          <TimerInput startInMinutes={startInMinutes} handleInput={handleInput} />         
-         <Timer minutes={minutes} seconds={seconds} />         
+         <Timer minutes={minutes} seconds={seconds} className={className} />         
       </div>
    );
 };
