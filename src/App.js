3a7ffa5f30c9c5d12644f1439d2f9c90d6882777
Renata Santoso <santoso.renata@gmail.com>
2020-07-26 08:22:03 -0700

    Update count down component import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./styles.css";
-import CountDown from "./Components/CountDown";
+import CountDown from "./Components/CountDown/CountDown";
 
 const App = () => {
    return <CountDown />;
