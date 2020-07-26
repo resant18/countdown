b13c9b239352afdeb2d7e4a3b94c9ac4b11056c3
Renata Santoso <santoso.renata@gmail.com>
2020-07-25 23:59:02 -0700

    Add count down time left functionality

--- src/Components/CountDown.js
+++ src/Components/CountDown.js
@@ -8,7 +8,9 @@ const CountDown = () => {
    const [seconds, setSeconds] = useState(0);
    const [isActive, setIsActive] = useState(false);
    const [isOver, setIsOver] = useState(false);
-   
+   const [isHalfway, setIsHalfway] = useState(false);
+   const [className, setClassName] = useState("");   
+
    let timeLeft;
 
    const handleInput = (e) => {
@@ -32,6 +34,28 @@ const CountDown = () => {
          setSeconds(timeLeft - min * 60);
          checkTimeLeft();
       }
+   };   
+
+   const checkTimeLeft = () => {
+      let mins = startInMinutes;
+      let halfTime = Math.floor((mins * 60) / 2);
+
+      timeLeft <= halfTime ? setIsHalfway(true) : setIsHalfway(false);
+
+      timeLeft <= 10
+         ? setClassName((className) => className + " blink")
+         : setClassName((className) => className.replace("blink", ""));
+      timeLeft <= 20
+         ? setClassName((className) => className + " red")
+         : setClassName((className) => className.replace("red", ""));
+   };
+
+   const renderNotification = () => {
+      if (isOver) {
+         return <h1>Time's up!</h1>;
+      } else if (isHalfway) {
+         return <h1>More than halfway there!</h1>;
+      } else return null;
    };
 
    useEffect(() => {
@@ -47,12 +71,13 @@ const CountDown = () => {
       }
 
       return () => clearInterval(timerId);
-   });
+   });   
 
    return (
       <div>
          <label htmlFor='timeInput'>CountDown: </label>         
          <TimerInput startInMinutes={startInMinutes} handleInput={handleInput} />         
+         {renderNotification()}
          <Timer minutes={minutes} seconds={seconds} className={className} />         
       </div>
    );
--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,21 +1,21 @@
 import React from "react";
 
-const Timer = ({ minutes, seconds }) => {
-   const formatMinutes = (minutes, digit) => {
-      if (digit === 2) return minutes < 10 ? "0" + minutes : minutes;
-      else return minutes;
-   };
+const Timer = ({ minutes, seconds, className }) => {
+  const formatMinutes = (minutes, digit) => {
+    if (digit === 2) return minutes < 10 ? "0" + minutes : minutes;
+    else return minutes;
+  };
 
-   const formatSeconds = (seconds, digit) => {
-      if (digit === 2) return seconds < 10 ? "0" + seconds : seconds;
-      else return seconds;
-   };
-   
-   return (
-      <div>
-         {formatMinutes(minutes, 2)} : {formatSeconds(seconds, 2)}
-      </div>
-   );
+  const formatSeconds = (seconds, digit) => {
+    if (digit === 2) return seconds < 10 ? "0" + seconds : seconds;
+    else return seconds;
+  };
+  
+  return (
+    <div className={className}>
+      {formatMinutes(minutes, 2)} : {formatSeconds(seconds, 2)}
+    </div>
+  );
 };
 
 export default Timer;
